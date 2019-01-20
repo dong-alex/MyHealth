144e1dd6c6343d3ee3bc934a651b6f4ca8bf9518
dong-alex <adong@ualberta.ca>
2019-01-19 22:07:02 -0700

    missing conflict

--- models/medication.js
+++ models/medication.js
@@ -2,7 +2,6 @@ const medication = (sequelize, DataTypes) => {
   const Medication = sequelize.define('medication', {
     medicationID: {
       type: DataTypes.INTEGER,
-<<<<<<< HEAD
       unique: true,
       primaryKey: true
     },
@@ -11,13 +10,6 @@ const medication = (sequelize, DataTypes) => {
     description: DataTypes.STRING,
     dosage: DataTypes.STRING,
     lastTaken: DataTypes.TIME
-=======
-      unique: true
-    },
-    date: DataTypes.DATEONLY,
-    name: DataTypes.STRING,
-    description: DataTypes.STRING
->>>>>>> 2618bd280ce0ce38a92f973ac062c2381b1e2771
   });
   // Medication.associate = (models) => {
   //   // Medication.belongsToMany(models.Patient, {
